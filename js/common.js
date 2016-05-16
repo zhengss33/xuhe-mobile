327e02d36680aa79f135cf1fd98f0a3ea02c6f9d
zhengss33 <493164072@qq.com>
2016-05-16 22:09:48 +0800

    index.html

---
+++ js/common.js
@@ -0,0 +1,19 @@
+(function (doc, win) {
+       var docEl = doc.documentElement,
+               resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+               recalc = function () {
+                   var clientWidth = docEl.clientWidth;
+                   if (!clientWidth) return;
+                   if(clientWidth >=1080){
+                	   clientWidth = 1080;
+                   }
+                   if (clientWidth <=240) {
+                    clientWidth = 240;
+                   }
+                   docEl.style.fontSize = clientWidth /10.8+'px';
+               };
+       if (!doc.addEventListener) return;
+       win.addEventListener(resizeEvt, recalc, false);
+       doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+ 
